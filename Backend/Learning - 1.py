dd00b75b4e6c878db819da6f82cd197561ccb284
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-14 00:36:48 -0400

    [200~Getting started learning Full stack programming in Spyder

---
+++ Backend/Learning - 1.py
@@ -0,0 +1,51 @@
+#Date 07/13/2024
+#Owner : Orzubek Kamariddinov
+
+#print("Hello world");
+
+#print(8-3)
+
+print("Assalomu Alaykum")
+
+print("Men \"Dell\" markasidagi Leptop sotib oldim")
+
+print('Assalomu alaykum')
+print('Men "Dell" markasidagi Leptop sotib oldim')
+
+print('Qo\'shtirnoqda foydalanish sir asrorlarini o\'rganish')
+
+
+print(""" Hello World
+      
+      va yana davom etish""")
+      
+      
+print("Matini ikkiga bo'lish bu \n Va bu bo'lindi")
+
+print(10+10*4)
+
+print(10/2)
+print(10//2)
+
+print(5**5)
+print(5*5)
+
+
+print("Harif bva sonlar aralashuvi deya olaman", 10*2, "Ko'paytirdimi")
+
+print('10*2=',10+10)
+
+name = "Orzubek"
+last = "Kamariddinov"
+yo = 19
+
+yo = 18
+print(name,last,"Yoshi",yo)
+
+
+
+true = 10
+false = 10
+
+c = (true + false)**2
+print(c)
---
+++ Backend/Learning - 2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 00:24:04 2024
+
+@author: qorzu
+"""
+
