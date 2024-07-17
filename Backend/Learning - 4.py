cbbc6ea30323677f0604c69d6292e86546a8b43e
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-16 23:26:00 -0400

    Consolidation and learning of lists

--- Backend/Learning - 4.py
+++ Backend/Learning - 4.py
@@ -1,7 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 15 23:43:14 2024
+radius = 10
+name = "Orzubek"
 
-@author: qorzu
-"""
+frutes = ['Apple', 'Anjir', 'Shaftoli', "o'rik"]
+
+son = [5, 20, 50, 60, 90, 14]
+
+aralash = [10, 20 , 'Orzu', 40, 'Orzubek']
+
+bosh = []
+
+print(son[2]) #Bu bilan chaqirish mumkun Listdagi qiymatlarni 0 dan boshlab
+print(frutes[1].upper())
+print(son[1] + son[0])
+frutes[0] = 'olma' #Bu soda o'zgartirish
+frutes.append('gilos') #Listga yangi qo'shimcha qo'shish Ro'yxatni oxiriga qoshadi
+frutes.insert(2,'Banan') #Listning kiritilgan indexsiga qarab xoxlagan joyga malumot joylsashtira oladi
+
+
+
+cars = ['Bmw', 'Mers', 'Audi', 'Ram']
+del cars[3] #Butunlay o'chirish uchun indexs kiritilsa bas
+
+cars.remove('Bmw') #Bu nomini yozsa  o'chiradi list ichidagini
+
+
+
+many = ["Yog", "Piyoz", "Kartoshka", "Go'sht"]
+
+
+bosh = many.pop(0) #Elementni butunligicha ko'chirib o'tqaish
+
 
---
+++ Backend/Learning - 5.py
@@ -0,0 +1,7 @@
+bosh = []
+
+ham = ["yog'", "piyoz", "kartoshka", "guruch"]
+
+bosh.append(ham.pop(1))
+print("Men olgan mahsulotlar:", bosh)
+print("Qolgan mahsulotlar:", ham)
