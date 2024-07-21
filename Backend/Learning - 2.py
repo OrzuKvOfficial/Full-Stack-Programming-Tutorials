63890a21e92c660dd4269fe9a937c973c754d639
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-20 21:43:22 -0400

    Sorting numbers and lists in Python

--- Backend/Learning - 2.py
+++ Backend/Learning - 2.py
@@ -1,17 +1,16 @@
-#Created on Sun Jul 14 00:24:04 2024
+# Created on Sun Jul 14 00:24:04 2024
 
 
 name = 'Orzubek'
 nLast = 'Kamariddinoov'
 
-#f"Mening Ismim {name})" Terminalga kiritilganda Matin yoki son qoshish mumkun
+# f"Mening Ismim {name})" Terminalga kiritilganda Matin yoki son qoshish mumkun
 
 fullname = f"{name} {nLast}"
 
 print(fullname)
 
 print(name + ' ' + nLast)
-
 
 
 print(f"Hello my name is {name}. My last name is {nLast}")
@@ -21,33 +20,37 @@ print('Hello World')
 print('Hello \tWorld')
 print('Hello \nWorld')
 
-#Upper
-print(fullname.upper()) #upper yangi qoshimcha katta harf qilib beradi.
-fullname = fullname.upper()  #Bu yerda upper O'zgaruvchini o'zgartiradi kattaga.
+# Upper
+print(fullname.upper())  # upper yangi qoshimcha katta harf qilib beradi.
+# Bu yerda upper O'zgaruvchini o'zgartiradi kattaga.
+fullname = fullname.upper()
 
-#Lower
-print(fullname.lower()) #lower hamma hariflarni kichik shakilga o'zgartiradi.
+# Lower
+print(fullname.lower())  # lower hamma hariflarni kichik shakilga o'zgartiradi.
 
-#Titele 
-print(fullname.title()) #Har bir so'zning bosh xarfini katta qiladi.
+# Titele
+print(fullname.title())  # Har bir so'zning bosh xarfini katta qiladi.
 
-#capitalize
-print((fullname.capitalize())) #Gapda birinchi xarifni katta qilib beradi
+# capitalize
+print((fullname.capitalize()))  # Gapda birinchi xarifni katta qilib beradi
 
-#Lstrip
+# Lstrip
 frutes = "   apple   "
 print(frutes)
-print(frutes.lstrip() + "Yaxshi ko'raman") #Chap tomondagi bo'shliqni olib tashlaydi
+# Chap tomondagi bo'shliqni olib tashlaydi
+print(frutes.lstrip() + "Yaxshi ko'raman")
 
-#Rstrip
-print(frutes.rstrip() + "Yaxshi ko'raman") #O'ng tarafdagi bo'shliqni olib tashlaydi
+# Rstrip
+# O'ng tarafdagi bo'shliqni olib tashlaydi
+print(frutes.rstrip() + "Yaxshi ko'raman")
 
-#Strip
-print(frutes.strip() + "Yaxshi ko'raman") #Ikki yondagi bo'shliqni olib tashlaydi.
- 
+# Strip
+# Ikki yondagi bo'shliqni olib tashlaydi.
+print(frutes.strip() + "Yaxshi ko'raman")
 
-#Malumootni qabul qilish
-manba = input("Ismingizni kiriting...") #Malumotlar kiritish mumkun
+
+# Malumootni qabul qilish
+manba = input("Ismingizni kiriting...")  # Malumotlar kiritish mumkun
 print("Assalomu Alaykum, " + manba)
 manba = input("Ismingiz nima?\n>>>>>")
 print("Assalomu alaykum, " + manba.title())
--- Backend/Learning - 6.py
+++ Backend/Learning - 6.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 18 21:52:04 2024
+cars = ["Toyota Camry", "Honda Accord", "Ford Mustang", "Chevrolet Corvette", "BMW 3 Series", "Mercedes-Benz C-Class", "Audi A4", "Tesla Model S"]
+numbers = [3, 7, 1, 9, 4, 12, 8, 2, 15, 6]
 
-@author: qorzu
-"""
+#orted(cars, reverse=True) Bu listga tegmay hammasini  
+#sorted(numbers, reverse=True) Bu list ichidagilariga tegmay ketma ket taxlaydi
+
+#cars.reverse() Bu list ichidi hamma narsani ketma ket qila oladi
+
+len(cars)
+
+print(numbers)
+print(cars)
+
+
 
