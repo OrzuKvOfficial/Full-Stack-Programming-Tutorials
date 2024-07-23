cbdeccf39fd333f3fdb54bd9d71232e11771d8a0
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-22 23:55:29 -0400

    Duplicate lists and jump to a new topic

--- Backend/Learning - 6.py
+++ Backend/Learning - 6.py
@@ -6,10 +6,41 @@ numbers = [3, 7, 1, 9, 4, 12, 8, 2, 15, 6]
 
 #cars.reverse() Bu list ichidi hamma narsani ketma ket qila oladi
 
-len(cars)
+sonlar = len(cars)
+
+qosh = list(range(0,12)) #Bu range so'zi sonlar qoshib beradi.
 
 print(numbers)
 print(cars)
 
+toq = list(range(1,50,2)) #Bu yerda qadamini belgilash mumkun 2 yozse u 1 ta tashlab toq son qiladi
+juft = list(range(0,20,2)) #Bu yerda qadamini belgilash mumkun 2 yozse u 1 ta tashlab juft son qiladi
 
+
+katta = max(toq)
+kichik = min(juft)
+
+sum(numbers) #Bu list ichidagi qiymatlarni bir biriga qoshadi.
+
+
+#Misol uchun 
+ 
+treder = [20, 10,200, 20, 20, 20, 20, 20,20]
+arzon = min(treder)
+qimat = max(treder)
+jami = sum(treder)
+
+print('Kichik qiymat', arzon, 'Katta qiymat', qimat, 'Hammasi bolib', jami)
+
+#List ichidan ajratib olish
+
+print(cars[0:3]) #List ichidan kesib olish
+
+
+#Listni nusxalash uchun
+
+moshinlar = cars
+
+cars.remove("Ford Mustang") #LIst ichidan keraaksiz malumotni kesib olish
+print(cars)
 
