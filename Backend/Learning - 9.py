5ee3d8bec7c2ca616e58064e304a6168f50b927d
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-07-26 23:36:15 -0400

    Difficult operations on lists in Python

---
+++ Backend/Learning - 9.py
@@ -0,0 +1,8 @@
+import math
+
+numbers = [1, 2, 3, 4]
+sum_of_numbers = sum(numbers)
+product_of_numbers = math.prod(numbers)
+
+print(sum_of_numbers)  # Output: 10
+print(product_of_numbers)  # Output: 24
