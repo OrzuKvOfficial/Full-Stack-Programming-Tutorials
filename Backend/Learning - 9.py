fa826002044ed7f574d18b9a78a17b21e6dc90cf
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-08-08 21:45:08 -0400

    Practice

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,13 +1,19 @@
 import socket
 
+# Server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("localhost", 12345))
-server_socket.listen()
+server_socket.bind(('0.0.0.0', 8080))
+server_socket.listen(1)
 
-conn, addr = server_socket.accept()
-print(f"Connected by {addr}")
+client_socket, address = server_socket.accept()
+print(f"Connection from {address} has been established.")
 
-data = conn.recv(1024)
-print("Received", repr(data))
+client_socket.send(bytes("Hello from server", "utf-8"))
+client_socket.close()
 
-conn.close()
+# Client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('your_server_ip', 8080))
+message = client_socket.recv(1024)
+print(message.decode("utf-8"))
+client_socket.close()
