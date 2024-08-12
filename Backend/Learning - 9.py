be5d479a62259f5b4f12d7ce5c6d556ad000b5ce
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-08-11 23:24:29 -0400

    Email management

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,7 +1,31 @@
-# Faylni ochish va har bir qatorni alohida o'qish
-with open('fayl_nomi.txt', 'r') as fayl:
-    qatorlar = fayl.readlines()
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# O'qilgan ma'lumotni chop etish
-for qator in qatorlar:
-    print(qator.strip())
+# Email ma'lumotlari
+sender_email = "your_email@gmail.com"
+receiver_email = "receiver_email@example.com"
+password = "your_password"
+
+# Email xabari tayyorlash
+message = MIMEMultipart()
+message['From'] = sender_email
+message['To'] = receiver_email
+message['Subject'] = "Email Subject"
+
+# Email mazmuni
+body = "Bu emailning mazmuni."
+message.attach(MIMEText(body, 'plain'))
+
+# SMTP serverga ulanish va email yuborish
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()  # TLS (Transport Layer Security) ni yoqish
+    server.login(sender_email, password)
+    text = message.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+    print("Email muvaffaqiyatli yuborildi.")
+except Exception as e:
+    print(f"Xato yuz berdi: {e}")
+finally:
+    server.quit()
