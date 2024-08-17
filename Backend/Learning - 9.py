b71dde9dfccaea556b4d271764ba8d5d5a67e4a7
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-08-16 23:48:27 -0400

    Check

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,22 +1,8 @@
-import cv2
+import psutil
 
-# Kamerani ulash
-cap = cv2.VideoCapture(0)
+def check_connected_devices():
+    devices = psutil.disk_partitions()
+    for device in devices:
+        print(f"Device: {device.device}, Mountpoint: {device.mountpoint}")
 
-while True:
-    # Video oqimidan ramkalarni o'qing
-    ret, frame = cap.read()
-    
-    if not ret:
-        break
-    
-    # Tasvirni ko'rsatish
-    cv2.imshow('Kamera', frame)
-    
-    # Agar 'q' tugmasi bosilsa, chiqish
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Kamerani yopish va barcha oynalarni o'chirish
-cap.release()
-cv2.destroyAllWindows()
+check_connected_devices()
