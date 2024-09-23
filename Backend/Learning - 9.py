fdf3536524ac12648699b62d05919072f0974b6b
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-09-22 23:02:15 -0400

    Proper use of the library

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,8 +1,27 @@
-my_list = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-# Birinchi qadam: barcha ichki listlarni tekislaymiz (flat)
-flat_list = [item for sublist in my_list for item in sublist]
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f'Fayl yaratildi: {event.src_path}')
+        
+    def on_modified(self, event):
+        print(f'Fayl o‘zgartirildi: {event.src_path}')
+        
+    def on_deleted(self, event):
+        print(f'Fayl o‘chirildi: {event.src_path}')
 
-# Yig'indi
-total = sum(flat_list)
-print("Yig'indisi:", total)
+if __name__ == "__main__":
+    path = "."  # Kuzatiladigan papka manzili (joriy papka)
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
