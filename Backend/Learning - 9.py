f33c22251b2a5a4370ffb105008f2bd3d2c4ce0b
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-10-01 23:24:08 -0400

    Determining if the printer is connected

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,25 +1,14 @@
-# Vazn va bo'y bo'yicha kunlik suv miqdorini hisoblash
-def daily_water_intake(weight_kg):
-    # Har bir kilogramm vazn uchun 30-35 ml suv ichish tavsiya qilinadi
-    # O'rtacha hisobda har kg uchun 32.5 ml suv ichish kerak
-    water_ml_per_kg = 32.5
-    daily_intake_ml = weight_kg * water_ml_per_kg  # kunlik suv miqdori (ml)
-    daily_intake_l = daily_intake_ml / 1000  # litrga o'tkazamiz
-    return daily_intake_l
+import win32print
 
-# Oylik suv miqdorini hisoblash
-def monthly_water_intake(weight_kg):
-    daily_intake_l = daily_water_intake(weight_kg)
-    monthly_intake_l = daily_intake_l * 30  # 1 oyda 30 kun bor deb hisoblaymiz
-    return monthly_intake_l
+def check_printers():
+    # Kompyuterga ulangan printerlar ro'yxatini olish
+    printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL | win32print.PRINTER_ENUM_CONNECTIONS)
 
-# Foydalanuvchi ma'lumotlarini kiritadi
-weight_kg = float(input("Vazningizni kilogrammda kiriting: "))
-height_cm = float(input("Bo'yingizni santimetrda kiriting: "))
+    if printers:
+        print("Quyidagi printerlar ulangan:")
+        for printer in printers:
+            print(f"Printer nomi: {printer[2]}")
+    else:
+        print("Printerlar topilmadi.")
 
-# Natijalarni hisoblash va chiqarish
-daily_water = daily_water_intake(weight_kg)
-monthly_water = monthly_water_intake(weight_kg)
-
-print(f"Sizning bir kunda ichishingiz kerak bo'lgan suv miqdori: {daily_water:.2f} litr.")
-print(f"Sizning bir oyda ichishingiz kerak bo'lgan suv miqdori: {monthly_water:.2f} litr.")
+check_printers()
