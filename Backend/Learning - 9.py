c71c5b13216038b1fc191721dfe8a00a6a876220
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-10-11 23:37:17 -0400

    Just Code Imgs

--- Backend/Learning - 9.py
+++ Backend/Learning - 9.py
@@ -1,27 +1,5 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+fmt = "{:>10.2f}"  # 10 belgidan iborat joy, 2ta kasr belgisi bilan, o'ngga tekislanadi
+numbers = [12.3, 45.6789, 0.4567]
 
-def send_email(to_email, subject, body):
-    # Gmail SMTP serverining ma'lumotlari
-    smtp_server = "smtp.gmail.com"
-    smtp_port = 587
-    sender_email = "your_email@gmail.com"
-    sender_password = "your_password"
-    
-    # Emailni tayyorlash
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-    
-    # Serverga ulanish va emailni yuborish
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(sender_email, sender_password)
-    server.sendmail(sender_email, to_email, msg.as_string())
-    server.quit()
-
-# Foydalanish
-send_email("student@example.com", "Yangi Dars Qo'shildi", "Siz uchun yangi dars qo'shildi.")
+for number in numbers:
+    print(fmt.format(number))
