0dbae4fad7e2469563319a4d9b9133c92f3f1b49
Orzubek Kamariddinov <qorzubek394@gmail.com>
2025-01-28 22:06:37 -0500

    Formatting

--- Backend/Learning - 10.py
+++ Backend/Learning - 10.py
@@ -1,16 +1,5 @@
-from datetime import datetime
-import pytz
+name = "Orzubek"
+age = 18
+city = "Samarqand"
 
-# Joylar va ularning vaqt zonalari
-locations = {
-    "New York": "America/New_York",
-    "London": "Europe/London",
-    "Tashkent": "Asia/Tashkent",
-    "Tokyo": "Asia/Tokyo"
-}
-
-# Har bir joy uchun hozirgi vaqtni ko'rsatish
-for city, timezone in locations.items():
-    local_time = datetime.now(pytz.timezone(timezone))
-    print(f"{city}: {local_time.strftime('%Y-%m-%d %H:%M:%S')}")
-
+print(f"Men {name}, yoshim {age}, {city} shahridanman.")
